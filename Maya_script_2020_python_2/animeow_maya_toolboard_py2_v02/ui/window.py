2791a4b1ca6904dad03fd436e43d4f56d6653098
tonynguyen030693 <48915231+tonynguyen030693@users.noreply.github.com>
2026-07-15 14:29:19 +0700

    fix(py2): prevent recursive __builtin__.print wrap-around on module reload

--- Maya_script_2020_python_2/animeow_maya_toolboard_py2_v02/ui/window.py
+++ Maya_script_2020_python_2/animeow_maya_toolboard_py2_v02/ui/window.py
@@ -4,24 +4,28 @@ from __future__ import print_function, absolute_import, division
 # Override __builtin__.print to safely handle unicode strings in Python 2
 try:
     import __builtin__
-    _orig_print = getattr(__builtin__, 'print', None)
-    if _orig_print is None:
-        def _orig_print(*args, **kwargs):
-            fp = kwargs.get('file', sys.stdout)
-            sep = kwargs.get('sep', ' ')
-            end = kwargs.get('end', '\n')
-            fp.write(sep.join(str(arg) for arg in args) + end)
+    current_print = getattr(__builtin__, 'print', None)
+    if current_print and getattr(current_print, '__name__', '') == 'safe_print':
+        pass
+    else:
+        _orig_print = current_print
+        if _orig_print is None:
+            def _orig_print(*args, **kwargs):
+                fp = kwargs.get('file', sys.stdout)
+                sep = kwargs.get('sep', ' ')
+                end = kwargs.get('end', '\n')
+                fp.write(sep.join(str(arg) for arg in args) + end)
 
-    def safe_print(*args, **kwargs):
-        safe_args = []
-        for arg in args:
-            if isinstance(arg, unicode):
-                safe_args.append(arg.encode('utf-8'))
-            else:
-                safe_args.append(arg)
-        _orig_print(*safe_args, **kwargs)
+        def safe_print(*args, **kwargs):
+            safe_args = []
+            for arg in args:
+                if isinstance(arg, unicode):
+                    safe_args.append(arg.encode('utf-8'))
+                else:
+                    safe_args.append(arg)
+            _orig_print(*safe_args, **kwargs)
 
-    __builtin__.print = safe_print
+        __builtin__.print = safe_print
 except Exception:
     pass
 
