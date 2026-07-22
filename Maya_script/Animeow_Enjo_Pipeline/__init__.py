7e8293f8572106c49097943cd488e27b81921fc6
tonynguyen030693 <48915231+tonynguyen030693@users.noreply.github.com>
2026-07-22 21:25:04 +0700

    fix(init): Lazy import window inside show() to eliminate any Maya Python 2 SyntaxError

--- Maya_script/Animeow_Enjo_Pipeline/__init__.py
+++ Maya_script/Animeow_Enjo_Pipeline/__init__.py
@@ -1,13 +1,8 @@
 # -*- coding: utf-8 -*-
-import sys
-
-import Animeow_Enjo_Pipeline.core.file_manager as file_manager
-import Animeow_Enjo_Pipeline.core.playblast_manager as playblast_manager
-import Animeow_Enjo_Pipeline.ui.window as window
-import Animeow_Enjo_Pipeline.menu as menu
 
 def show():
     """Ham khoi tao chinh de hien thi UI tu Shelf hoac Menu"""
+    from Animeow_Enjo_Pipeline.ui import window
     window.show_window()
 
 
