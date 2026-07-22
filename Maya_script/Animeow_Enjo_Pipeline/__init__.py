3634adad33ce07f391768d11a2bd44dab99d28b5
tonynguyen030693 <48915231+tonynguyen030693@users.noreply.github.com>
2026-07-22 21:18:39 +0700

    fix(installer): Streamline __init__.py and install.mel for clean sys.modules reload

--- Maya_script/Animeow_Enjo_Pipeline/__init__.py
+++ Maya_script/Animeow_Enjo_Pipeline/__init__.py
@@ -1,24 +1,13 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function, absolute_import, division
-
 import sys
 
-def force_reload():
-    """Xoa sach cache sys.modules cua Animeow_Enjo_Pipeline de dam bao load code python moi 100%"""
-    mod_prefix = "Animeow_Enjo_Pipeline"
-    modules_to_del = [k for k in sys.modules if k.startswith(mod_prefix)]
-    for k in modules_to_del:
-        try:
-            del sys.modules[k]
-        except KeyError:
-            pass
-
-from Animeow_Enjo_Pipeline.core import file_manager
-from Animeow_Enjo_Pipeline.core import playblast_manager
-from Animeow_Enjo_Pipeline.ui import window
-from Animeow_Enjo_Pipeline import menu
+from .core import file_manager
+from .core import playblast_manager
+from .ui import window
+from . import menu
 
 def show():
     """Ham khoi tao chinh de hien thi UI tu Shelf hoac Menu"""
     window.show_window()
+
 
