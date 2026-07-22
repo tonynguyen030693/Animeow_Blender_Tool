cda0f8aefb3103897de300c9281d83ab6a8a7eef
tonynguyen030693 <48915231+tonynguyen030693@users.noreply.github.com>
2026-07-22 21:21:37 +0700

    fix(python2): Use standard absolute import aliases in __init__.py to prevent Maya 2020 SyntaxError

--- Maya_script/Animeow_Enjo_Pipeline/__init__.py
+++ Maya_script/Animeow_Enjo_Pipeline/__init__.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
-
 import sys
 
-from .core import file_manager
-from .core import playblast_manager
-from .ui import window
-from . import menu
+import Animeow_Enjo_Pipeline.core.file_manager as file_manager
+import Animeow_Enjo_Pipeline.core.playblast_manager as playblast_manager
+import Animeow_Enjo_Pipeline.ui.window as window
+import Animeow_Enjo_Pipeline.menu as menu
 
 def show():
     """Ham khoi tao chinh de hien thi UI tu Shelf hoac Menu"""
